a67ed774db857b9c19b74bd9ee83e7d77260f738
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-04-14 22:12:38 -0400

    fixed error in loop to show numbers and special characters in password

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-//the charateristics of the passwordgi
+//the charateristics of the password
 const alpha = 'ABCDEFGHIJKLMOPQRSTUVWXYZ';
 const numbers = '0123456789';
-const special = '@#!%-$+'
+const special = '@#!%-$+';
 let len = 0;
 let temp = '';
 let password = '';
@@ -13,53 +13,48 @@ document.querySelector('button').addEventListener('click', handleClick);
 function handleClick() {
   temp = ''
   len = prompt("How long do you want your password to be?(8-128)");
-  
-  if (len < 8 ) {
+
+  if (len < 8) {
     alert("Length is too short");
     return handleClick();
   };
-  if (len > 128){
+  if (len > 128) {
     alert("Length is too long");
     return handleClick();
   };
   if (isNaN(len)) {
-    alert ("That is invalid, please choose a numerical value");
+    alert("That is invalid, please choose a numerical value");
     return handleClick();
   };
 
   let upper = confirm('Would you like uppercase letter?');
 
-  if (upper){
+  if (upper) {
     temp += alpha;
   };
 
   let lower = confirm("Would you like lowercase letter?");
-  
+
   if (lower) {
     temp += alpha.toLowerCase();
   };
 
-  let special = alert("Would you like a special character?"); 
+  let spec = confirm("Would you like a special character?");
 
-  if (special) {
+  if (spec) {
     temp += special;
   }
 
-  let numbers = alert("Would like a numerical value?");
+  let num = confirm("Would like a numerical value?");
 
-  if(numbers) {
+  if (num) {
     temp += numbers;
   }
+  //gets the random password in the console log
 
-
-  
-//gets the random password in the console log
- 
   for (let i = 0; i < len; i++) {
-    let generate = temp[Math.floor(Math.random()*temp.length)];
-    console.log(generate);
-    
- }
-
-
+    password += temp[Math.floor(Math.random() * temp.length)];
+  };
+  
+  document.getElementById("password").innerText = password;
 }
